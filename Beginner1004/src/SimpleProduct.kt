8be46a336c03fd267c822775a8a7b12422738269
anadunning <anakpedrosa@hotmail.com>
2019-11-06 10:45:16 -0300

    Simple Product

---
+++ Beginner1004/src/SimpleProduct.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+fun main() {
+
+    val reader = Scanner(System.`in`)
+
+    var a:Int = reader.nextInt()
+    var b:Int = reader.nextInt()
+
+    var prod:Int
+
+    prod = a * b
+
+    println("PROD = $prod")
+
+}
