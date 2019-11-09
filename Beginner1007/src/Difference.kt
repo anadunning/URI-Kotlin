cc5cc8cd3db2c68dbbd346f8c137da1102c4e366
anadunning <anakpedrosa@hotmail.com>
2019-11-09 13:59:44 -0300

    Difference

---
+++ Beginner1007/src/Difference.kt
@@ -0,0 +1,17 @@
+import java.util.*
+
+fun main() {
+
+    val reader = Scanner(System.`in`)
+
+    var a:Int = reader.nextInt()
+    var b:Int = reader.nextInt()
+    var c:Int = reader.nextInt()
+    var d:Int = reader.nextInt()
+
+    var Dif:Int
+
+    Dif = (a * b) - (c * d)
+
+    println("DIFERENCA = $Dif")
+}
